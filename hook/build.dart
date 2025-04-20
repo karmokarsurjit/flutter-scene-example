3f68a454a55bd5a56f69e2fe987c26a3797c74e4
karmokarsurjit <karmokarsurjit@gmail.com>
2025-04-20 15:43:45 +0600

    Add Flutter scene importer build hook, Change config of buildConfig to input of buildInput as of change of dependencies.

--- hook/build.dart
+++ hook/build.dart
@@ -1,9 +1,9 @@
-import 'package:native_assets_cli/native_assets_cli.dart';
 import 'package:flutter_scene_importer/build_hooks.dart';
+import 'package:native_assets_cli/native_assets_cli.dart';
 
 void main(List<String> args) {
-  build(args, (config, output) async {
-    buildModels(buildConfig: config, inputFilePaths: [
+  build(args, (input, output) async {
+    buildModels(buildInput: input, inputFilePaths: [
       'models/coin.glb',
       'models/dash.glb',
       'models/ground.glb',
